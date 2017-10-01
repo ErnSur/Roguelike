2a1c69e2827b27d26d31e2631276cc04f22d24d0
ErnSur <ernest.surys@gmail.com>
2017-10-01 20:54:03 +0200

    update

---
+++ Roguelike/Roglik/Assets/Scripts/FiniteStateMachine/StateController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateController : MonoBehaviour {
+
+    public State currentState;
+    public NPCstats npcStats;
+    public PFaStar pathfinding;
+
+    private bool aiActive;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!aiActive)
+            return;
+
+        currentState.UpdateState(this);
+	}
+}
---
+++ Roguelike/Roglik/Assets/Scripts/ScriptableObjects/NPC/NPCstats.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (menuName = "NPC/Stats")]
+public class NPCstats : ScriptableObject {
+
+    public int maxHp;
+    public int currentHp;
+
+    public int attackDamage;
+    public int visionRange;
+
+}
